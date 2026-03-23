58135ef69439ede4df9f937fd1453c44c5e5657a
CooperS11 <csvart11@gmail.com>
2026-03-23 09:22:20 -0700

    updates

--- src/main/java/com/LHSprojects/TCLHS/TclhsApplication.java
+++ src/main/java/com/LHSprojects/TCLHS/TclhsApplication.java
@@ -10,17 +10,7 @@ public class TclhsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TclhsApplication.class, args);
 
-			//waiting for declarations to be fixed
-			Repository repo = new Repository(); 
-			System.out.println(repo.getAllTutors().size());
 			
-			for (int i = 0; i < repo.getAllTutors().size(); i++) {
-				if (i = repo.getAllTutors().size() - 2) {
-					System.out.print(repo.getAllTutors.get(i));
-				} else {
-					System.out.print(repo.getAllTutors.get(i) + ", ");
-				}
-			}
 
 	}
 
